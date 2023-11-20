e6328bd9d5a083faab1da6bbec3d7e0b7def25eb
SteveSplash934 <stevesplash4@gmail.com>
2023-11-20 10:38:19 -0800

    fourth commit

--- watch.py
+++ watch.py
@@ -1,3 +1,18 @@
+'''
+S3ns1W47ch
+BORED AND OUT MY MIND, THEN I WROTE A SIMPLE SCRIPT TO WATCH WHAT'S HAPPENING ON MY PC FROM MY PHONE OR OTHER COMPUTERS AT HOME!
+USE FOR GOOD ONLY! REMEMBER GOD IS WATCHING YOU;
+ONLY FOR EDUCATIONAL PURPOSE ONLY
+
+DEVELOPER: SHEPHERDDOMAIN (aka. SteveSplash)
+
+CONTACTS: {
+    'GitHub': 'https://github.com/stevesplash934/',
+    'Gmail': 'stevesplash4@gmail.com',
+    'Facecbook': 'https://facebook.com/steve.splash.10'
+}
+'''
+
 from flask import Flask, Response, send_file
 from PIL import ImageGrab
 import cv2
@@ -6,13 +21,17 @@ import random as rdm
 from datetime import datetime
 import string as stg
 import argparse
-# import ngork
+import ngrok
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--interface', default='127.0.0.1', help='Host/Interface on which the service will be hosted on.')
 parser.add_argument('-p', '--port', default=9090, help='Port on which the host will be running on.')
+parser.add_argument('-ng', '--ngrok', help='Enable ngrok tunneling, pass your authkey or else ngrok won\'t work')
 args = parser.parse_args()
 
+if args.ngrok and args.ngrok == '':
+    print('NGROK authkey missing! Quitting program.')
+    exit()
 
 def new_video_filename():
     '''
@@ -27,6 +46,10 @@ def new_video_filename():
 
 HOST = args.interface or '127.0.0.1'
 PORT = args.port or 9090
+
+if args.ngrok:
+    AUTHTOKEN = args.ngrok
+    
 VIDEO_FILENAME = new_video_filename() # generate a video file name
 print(VIDEO_FILENAME)
 
@@ -68,5 +91,10 @@ def download_video():
         print(e)
         
 if __name__ == "__main__":
-    # add ngrok tunneling here
-    app.run(host=HOST, port=PORT, debug=True)
+    if args.ngrok:
+        try:
+            listener = ngrok.connect(PORT, authtoken=AUTHTOKEN)
+            print (f"Loopback URL established at: {listener.url()}")
+        except Exception as e:
+            print(e)
+    app.run(host=HOST, port=PORT)
